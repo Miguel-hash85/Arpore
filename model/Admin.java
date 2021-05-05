7058070f34d50e9ff7926219d044010e78e6c2e9
1dami <1dami@TartangaLH.eus>
2021-05-05 09:05:54 +0200

    Clase Admin

--- model/Admin.java
+++ model/Admin.java
@@ -1,14 +1,14 @@
 package model;
 
 public class Admin extends User {
-	private String cod_adminString;
+	private String cod_admin;
 
 	// Setters and Getters
-	public String getCod_adminString() {
-		return cod_adminString;
+	public String getCod_admin() {
+		return cod_admin;
 	}
 
-	public void setCod_adminString(String cod_adminString) {
-		this.cod_adminString = cod_adminString;
+	public void setCod_adminString(String cod_admin) {
+		this.cod_admin = cod_admin;
 	}
 }
